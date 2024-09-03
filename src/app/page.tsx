b862918803e599b1700470df3391a23408fbdee0
mRahulRoy <71833197+mRahulRoy@users.noreply.github.com>
2024-09-03 23:14:17 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,7 +62,7 @@ export default function Home() {
         <div className="container   p-4 ">
          {/*  <Image alt="logo" height={"240"} width={240} src={"/Astrix Logo.png"} />  */}
           <Link href="/">
-            <Image alt="logo" height={"240"} width={240} src={null} />
+            <Image alt="logo" height={"240"} width={240} src={users[0].images} />
           </Link>
         </div>
         {/* This hero section is the animated component that we see at very first */}
